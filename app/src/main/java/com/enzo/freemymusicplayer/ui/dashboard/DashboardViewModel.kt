fa9918e8a9e7ee87960df779e32f8e8df4bf0e8f
emelenthia <p0amberayuayu@gmail.com>
2025-08-09 16:25:01 +0900

    Initial commit

---
+++ app/src/main/java/com/enzo/freemymusicplayer/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.enzo.freemymusicplayer.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
